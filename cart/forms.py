2233470b3092a98169a92b7a86745f9e4b0143e9
Inez <iminezj@gmail.com>
2020-03-10 14:30:04 +0100

    Added cart form for adding items to cart

--- cart/forms.py
+++ cart/forms.py
@@ -4,5 +4,5 @@ from django import forms
 PRODUCT_QUANTITY_CHOICES = [str(i) for i in range (1, 11)]
 
 class CartAddNewProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, corerce=int)
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
