4c350c0bd9e44276e87face1dd58d8171b5667ae
Inez <iminezj@gmail.com>
2020-03-12 00:35:50 +0100

    Changed order of url paths

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('products/<int:id>/<slug:slug>', views.product_details, name='product_details'),
+
+
+    path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('', views.home, name='home')
+    path('products/<int:id>/<slug:slug>', views.product_details, name='product_details'),
 ]
