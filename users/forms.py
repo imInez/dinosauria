801c27218a145959b99e79cf230c6cbd16e69c65
Inez <iminezj@gmail.com>
2020-03-12 14:28:22 +0100

    Added not mandatory fields to registration form

--- users/forms.py
+++ users/forms.py
@@ -4,14 +4,17 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    name = forms.CharField(max_length=20)
-    surname = forms.CharField(max_length=40)
-    street = forms.CharField(max_length=50)
-    building_flat = forms.CharField(max_length=10)
-    city = forms.CharField(max_length=50)
-    zipcode = forms.CharField(max_length=10)
+    name = forms.CharField(max_length=20, required=False)
+    surname = forms.CharField(max_length=40, required=False)
+    street = forms.CharField(max_length=50, required=False)
+    building_flat = forms.CharField(max_length=10, required=False)
+    city = forms.CharField(max_length=50, required=False)
+    zipcode = forms.CharField(max_length=10, required=False)
 
     class Meta:
         model = User
+        widgets = {'username': forms.HiddenInput()}
         fields = ['email', 'password1', 'password2',
                   'name', 'surname', 'street', 'building_flat', 'city', 'zipcode']
+
+
