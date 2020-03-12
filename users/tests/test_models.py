5b9c1a2da6f2aa3cdc192475f53e99305651e224
Inez <iminezj@gmail.com>
2020-03-12 11:43:12 +0100

    First user model unit tests

---
+++ users/tests/test_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from helpers.tests_helpers import create_test_user
+
+User = get_user_model()
+
+class UserModelTest(TestCase):
+
+    def test_creating_new_user(self):
+        create_test_user(user_model=User)
+        self.assertEqual(len(User.objects.all()), 1)
+        self.assertEqual(User.objects.first().email, 'testing1@random.com')
+
+    def test_email_uniqueness(self):
+        create_test_user(user_model=User, create_one=False)
+        self.assertEqual(len(User.objects.all()), 3)
+        self.assertNotEqual(User.objects.all()[0].email, User.objects.all()[1].email)
+
