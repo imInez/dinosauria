678f17193aa8c03e509d9eab41716c5b895192fa
Inez <iminezj@gmail.com>
2020-03-14 00:18:34 +0100

    Added cart form to shop views and templates

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,17 @@
 from .models import Product
 from django.shortcuts import render, get_object_or_404
-
+from cart.forms import CartAddProductForm
 
 def home(request):
     return render(request, 'shop/home.html')
 
 def products(request):
     products = Product.objects.filter(available=True)
-    return render(request, 'shop/product/product-list.html', {'products': products})
+    cart_form = CartAddProductForm()
+    return render(request, 'shop/product/product-list.html', {'products': products, 'cart_form': cart_form})
 
 def product_details(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    # form to add to cart
-    return render(request, 'shop/product/product-details.html', {'product': product})
+    cart_form = CartAddProductForm()
+    return render(request, 'shop/product/product-details.html', {'product': product, 'cart_form': cart_form})
 
