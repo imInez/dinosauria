ed605d5ebd49f6a1743cd0a6800f07ca8923467a
Inez <iminezj@gmail.com>
2020-03-16 17:55:27 +0100

    Created Profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=40)
+    street = models.CharField(max_length=50)
+    building_flat = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=10)
 
 
--- users/views.py
+++ users/views.py
@@ -2,19 +2,35 @@ from django.shortcuts import render, redirect
 from users.forms import UserRegisterForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth import views as auth_views
-
+from .models import Profile
 
 # Create your views here.
+
+
+
+def create_user_profile(user, cd):
+    profile = Profile()
+    profile.user = user
+    profile.name = cd.get('name')
+    profile.surname = cd.get('surname')
+    profile.street = cd.get('street')
+    profile.building_flat = cd.get('building_flat')
+    profile.city = cd.get('city')
+    profile.zipcode = cd.get('zipcode')
+    profile.save()
+
 
 def register(request):
     if request.method == 'POST':
         nxt = request.POST.get('next')
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            tmp = form.save(commit=False)
-            tmp.username = form.cleaned_data.get('email')
-            tmp.save()
-            auth_views.auth_login(request, tmp)
+            usr = form.save(commit=False)
+            cd = form.cleaned_data
+            usr.username = cd.get('email')
+            usr.save()
+            create_user_profile(usr, cd)
+            auth_views.auth_login(request, usr)
             return redirect(nxt)
     else:
         form = UserRegisterForm()
