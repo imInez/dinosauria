60dee50ddc07c31964764c2553b21e1fe77aabd3
Inez <iminezj@gmail.com>
2020-03-16 17:59:27 +0100

    Changed profile fields to not mandatory

--- users/models.py
+++ users/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=40)
-    street = models.CharField(max_length=50)
-    building_flat = models.CharField(max_length=10)
-    city = models.CharField(max_length=50)
-    zipcode = models.CharField(max_length=10)
+    name = models.CharField(max_length=20, blank=True)
+    surname = models.CharField(max_length=40, blank=True)
+    street = models.CharField(max_length=50, blank=True)
+    building_flat = models.CharField(max_length=10, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    zipcode = models.CharField(max_length=10, blank=True)
 
 
