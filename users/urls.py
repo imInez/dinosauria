2df5d0afb736a7f0446de1b26350d1a7dde0b24b
Inez <iminezj@gmail.com>
2020-03-17 11:02:34 +0100

    Created url and view for profile, and redirection to profile when user logsin from register page

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ app_name = 'users'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/auth/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/auth/logout.html'), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/auth/logout.html'), name='logout'),
+    path('profile/<user_id>/', views.profile, name='profile')
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from users.forms import UserRegisterForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth import views as auth_views
 from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -21,6 +22,8 @@ def create_user_profile(user, cd):
 
 
 def register(request):
+    if request.user.is_authenticated:
+        return profile(request, request.user.id)
     if request.method == 'POST':
         nxt = request.POST.get('next')
         form = UserRegisterForm(request.POST)
@@ -35,3 +38,9 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/auth/registration.html', {'form': form})
+
+
+def profile(request, user_id):
+    user = User.objects.filter(id=user_id)
+    user_profile = Profile.objects.filter(user=user)
+    return render(request, 'users/profile.html', {'user': user, 'profile': user_profile})
