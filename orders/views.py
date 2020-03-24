ed7a5eb5c3634be0e50a7b35d6a0cc391fea1eeb
Inez <iminezj@gmail.com>
2020-03-24 17:32:07 +0100

    Clearing session details data after order is made

--- orders/views.py
+++ orders/views.py
@@ -3,30 +3,33 @@ from .models import Order, OrderProduct
 from cart.cart import Cart
 from users.models import ShipmentAddress
 from users.views import get_profile
+from django.views.decorators.http import require_POST
 
-import time
 
+@require_POST
 def create_order(request):
     cart = Cart(request)
-    if request.method == 'POST':
-        profile = get_profile(request.user.email) if request.user.is_authenticated \
-            else get_profile(request.session.get('guest_profile_email'))
-        new_order = Order()
-        new_order.author = profile
-        new_order.address = ShipmentAddress.objects.filter(id=request.session.get('address')).first()
-        new_order.status = 'NEW'
-        new_order.total = cart.get_total_price()
-        new_order.save()
+    profile = get_profile(request.user.email) if request.user.is_authenticated \
+        else get_profile(request.session.get('guest_profile_email'))
+    new_order = Order()
+    new_order.author = profile
+    new_order.address = ShipmentAddress.objects.filter(id=request.session.get('address')).first()
+    new_order.status = 'NEW'
+    new_order.total = cart.get_total_price()
+    new_order.save()
 
-        for product in cart:
-            new_order_product = OrderProduct(order=new_order, product=product.get('product'),
-                                             price=product.get('price'), quantity=product.get('quantity'))
-            new_order_product.save()
-            new_order.products.add(new_order_product)
+    for product in cart:
+        new_order_product = OrderProduct(order=new_order, product=product.get('product'),
+                                         price=product.get('price'), quantity=product.get('quantity'))
+        new_order_product.save()
+        new_order.products.add(new_order_product)
 
-        new_order.save()
-        cart.clear()
+    new_order.save()
+    cart.clear()
+    del request.session['guest_profile_email']
+    del request.session['address']
+    del request.session['address']
 
-        return render(request, 'orders/success.html')
-    else:
-        pass
+
+    return render(request, 'orders/success.html')
+
